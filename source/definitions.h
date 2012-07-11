7e520fb5ff99d445311c39087e5f70e7c56d9311
Hampus Joakim Nilsson <mail@hjnilsson.com>
2012-07-11 18:25:42 +0200

    Changed version to 2.2

--- source/definitions.h
+++ source/definitions.h
@@ -22,8 +22,8 @@
 
 // Version info
 // xxyyzzt (major, minor, subversion)
-#define __RME_VERSION_MAJOR__      3
-#define __RME_VERSION_MINOR__      0
+#define __RME_VERSION_MAJOR__      2
+#define __RME_VERSION_MINOR__      2
 #define __RME_SUBVERSION__         0
 
 #define __LIVE_NET_VERSION__       5
