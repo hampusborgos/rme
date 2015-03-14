ef7d9f5470ca2ba1e8ca2f0d27351e767532858c
Nailson <nailsonnego@gmail.com>
2015-03-14 13:40:47 -0300

    Removed format 10.21
    Nothing has been changed in this version.

--- source/client_version.cpp
+++ source/client_version.cpp
@@ -244,14 +244,12 @@ void ClientVersion::loadVersion(pugi::xml_node versionNode)
 				client_data.datFormat = DAT_FORMAT_96;
 			} else if (format == "10.10") {
 				client_data.datFormat = DAT_FORMAT_1010;
-			} else if (format == "10.21") {
-				client_data.datFormat = DAT_FORMAT_1021;
 			} else if (format == "10.50") {
 				client_data.datFormat = DAT_FORMAT_1050;
 			} else if (format == "10.57") {
 				client_data.datFormat = DAT_FORMAT_1057;
 			} else {
-				wxLogError(wxT("Node 'data' 'format' is invalid (7.4, 7.55, 7.8, 8.6, 9.6, 10.10, 10.21, 10.50 and 10.57 are supported)"));
+				wxLogError(wxT("Node 'data' 'format' is invalid (7.4, 7.55, 7.8, 8.6, 9.6, 10.10, 10.50 and 10.57 are supported)"));
 				continue;
 			}
 
